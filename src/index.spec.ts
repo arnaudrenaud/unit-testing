2d99280bc857728652bc1ba4028c4bb0e5db58b9
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-05-15 11:21:51 +0200

    Add rule: shipping cost is free if total articles price >= 100

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,25 @@
 import { Article, getShippingCost } from ".";
 
 describe("getShippingCost", () => {
-  const articles: Article[] = [
-    { name: "Table", price: 300, weightG: 20000, quantity: 1 },
-    { name: "Chair", price: 90, weightG: 4500, quantity: 4 },
-  ];
+  describe("when articles total price is greater than or equal to 100 (10000 cents)", () => {
+    const articles: Article[] = [
+      { name: "Table", price: 30000, weightG: 20000, quantity: 1 },
+      { name: "Chair", price: 9000, weightG: 4500, quantity: 4 },
+    ];
 
-  it("returns 10 euros per kilogram of total weight (i.e. 1 cent per gram)", () => {
-    expect(getShippingCost(articles)).toEqual(20000 * 1 + 4500 * 4 * 1);
+    it("returns 0", () => {
+      expect(getShippingCost(articles)).toEqual(0);
+    });
+  });
+
+  describe("when articles total price is less than 100 (10000 cents)", () => {
+    it("returns 10 euros per kilogram of total weight (i.e. 1 cent per gram)", () => {
+      const articles: Article[] = [
+        { name: "Salt", price: 300, weightG: 150, quantity: 2 },
+        { name: "Chair", price: 9000, weightG: 4500, quantity: 1 },
+      ];
+
+      expect(getShippingCost(articles)).toEqual(150 * 2 + 4500 * 1);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ export type Article = {
 };
 
 export function getShippingCost(articles: Article[]): number {
+  const totalArticlesPrice = articles.reduce(
+    (cost, article) => cost + article.price * article.quantity,
+    0
+  );
+  if (totalArticlesPrice >= 10000) {
+    return 0;
+  }
   return articles.reduce(
     (cost, article) => cost + article.weightG * article.quantity,
     0
